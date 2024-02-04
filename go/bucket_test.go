e054abcae2d0dc3c064e65431cb4a2547abdea31
Alexander Gitter <contact@agitter.net>
2024-02-04 09:55:00 +0100

    Bucket: Add test for splitting

--- go/bucket_test.go
+++ go/bucket_test.go
@@ -59,3 +59,35 @@ func TestBucketGetEntryByIdOrReturnAll(t *testing.T) {
 		t.Error("Expected all entries")
 	}
 }
+
+func TestSplitAt(t *testing.T) {
+	var bucket = newBucket(8)
+	var nodeId1 = hexStringToNodeId("0000000000000000000000000000000000000001")
+	var nodeId2 = hexStringToNodeId("f000000000000000000000000000000000000002")
+	var nodeId3 = hexStringToNodeId("0000000000000000000000000000000000000003")
+	var nodeId4 = hexStringToNodeId("0000000000000000000000000000000000000004")
+	var nodeId5 = hexStringToNodeId("f000000000000000000000000000000000000005")
+
+	bucket.addEntry(dhtNode{nodeId: nodeId1})
+	bucket.addEntry(dhtNode{nodeId: nodeId2})
+	bucket.addEntry(dhtNode{nodeId: nodeId3})
+	bucket.addEntry(dhtNode{nodeId: nodeId4})
+	bucket.addEntry(dhtNode{nodeId: nodeId5})
+
+	var zero, one = bucket.splitAt(0)
+	var query = hexStringToNodeId("0000000000000000000000000000000000000000")
+	var result, exactMatch = zero.getEntryByIdOrReturnAll(query)
+	if exactMatch || len(result) != 3 {
+		t.Error("Expected zero to have 3 entries")
+	}
+	result, exactMatch = one.getEntryByIdOrReturnAll(query)
+	if exactMatch || len(result) != 2 {
+		t.Error("Expected one to have 2 entries")
+	}
+	if !zero.containsNodeId(nodeId1) {
+		t.Error("Expected zero to contain nodeId1")
+	}
+	if !one.containsNodeId(nodeId2) {
+		t.Error("Expected one to contain nodeId2")
+	}
+}
